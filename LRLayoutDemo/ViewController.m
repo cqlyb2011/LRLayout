2f97f8c06886f51ac34e574f3c2bdc25ad7a6ef6
LiYeBiao <liyebiao@Electronicss-Mac-mini.local>
2015-04-14 14:09:57 +0800

    Initial Commit

---
+++ LRLayoutDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LRLayoutDemo
+//
+//  Created by LiYeBiao on 15/4/14.
+//  Copyright (c) 2015年 GaoJing Electric Co., Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LRLayoutDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LRLayoutDemo
+//
+//  Created by LiYeBiao on 15/4/14.
+//  Copyright (c) 2015年 GaoJing Electric Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
